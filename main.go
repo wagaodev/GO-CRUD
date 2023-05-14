7f567944fc91f9e940cd5040c46bc9496c611527
wagaodev <wcfx.dev@gmail.com>
2023-05-14 02:35:03 -0300

    feat: using env

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"api/api/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main(){
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		panic("Falha ao carregar o arquivo .env")
+	}
+
+	app := gin.Default()
+
+	routes.AppRoutes(app)
+
+	// get the environment variable of APP_HOST
+	appHost := os.Getenv("APP_HOST")
+	if appHost == "" {
+		panic("Falha ao obter a variável de ambiente APP_HOST")
+	}
+
+	app.Run(appHost)
+}
